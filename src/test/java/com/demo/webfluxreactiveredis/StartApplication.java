6cdbeb94a0680b8c8f424fbf098fa86d2e6836e1
nomoreFt <hyunwoo.kim1108@gmail.com>
2023-07-21 21:36:12 +0900

    feat : test main class

---
+++ src/test/java/com/demo/webfluxreactiveredis/StartApplication.java
@@ -0,0 +1,10 @@
+package com.demo.webfluxreactiveredis;
+
+import org.springframework.boot.SpringApplication;
+
+public class StartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxReactiveRedisApplication.class, args);
+    }
+
+}
